5d4f801cdf3481c7c56096c3b216c419fc191567
bachardaowd <bachar.daowd@gmail.com>
2024-06-13 07:28:30 +0200

    create BrandServiceImp

---
+++ src/main/java/com/bachar/e_commerce/service/BrandServiceImp.java
@@ -0,0 +1,42 @@
+package com.bachar.e_commerce.service;
+
+import com.bachar.e_commerce.entity.Brand;
+import com.bachar.e_commerce.model.BrandResponse;
+import com.bachar.e_commerce.repository.BrandRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class BrandServiceImp implements BrandService {
+
+    private final BrandRepository brandRepository;
+
+    public BrandServiceImp(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+
+    @Override
+    public List<BrandResponse> getAllBrands() {
+        log.info("Fetching all brands!!!!");
+        // fetch brands
+        List<Brand> brandsList = brandRepository.findAll();
+        //Use stream operator to map with response
+        List<BrandResponse> brandResponse = brandsList.stream()
+                .map(this::converToBrandResponse)
+                .collect(Collectors.toList());
+        log.info("All brands are fetched!!!!");
+        return brandResponse;
+    }
+
+    private BrandResponse converToBrandResponse(Brand brand) {
+        return BrandResponse.builder().Id(brand.getId())
+                .name(brand.getName())
+                .build();
+    }
+}
