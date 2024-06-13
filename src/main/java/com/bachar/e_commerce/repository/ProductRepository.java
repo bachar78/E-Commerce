9c1ba68820628941976c0a1016ac31c7914427bb
bachardaowd <bachar.daowd@gmail.com>
2024-06-13 12:48:38 +0200

    Create Specification

--- src/main/java/com/bachar/e_commerce/repository/ProductRepository.java
+++ src/main/java/com/bachar/e_commerce/repository/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.bachar.e_commerce.repository;
 
 import com.bachar.e_commerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
+    Specification<Product> searchByNameContaining(String keyword);
+    Specification<Product> findByBrandId(int brandId);
+    Specification<Product> findByTypeId(int typeId);
+    Specification<Product> findByBrandAndType(int brandId, int typeId);
+
 }
